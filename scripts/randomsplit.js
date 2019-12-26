da8f529b13b24e225241bf2618a8e08899478175
Chris Hall <chrishallhome@gmail.com>
2019-12-26 10:28:19 +0000

    Player pool input by form

--- scripts/randomsplit.js
+++ scripts/randomsplit.js
@@ -1,50 +1,67 @@
-//generate a random integer below the max permissable value
-function generateRandom(min, max) {
-    return Math.ceil( Math.random() * ( max - min ) );
-}
+ 
+ ((d) => {
 
-//populate array with random integers
-//test that integer does not already exist
-//return array
-function generatePlayerIndex(players) {
-    let playerNumbers = [];
-    while ( playerNumbers.length < players ) {
-        var newIndex = (generateRandom(0, players)) - 1;
-        if (playerNumbers.indexOf(newIndex) == -1) {
+    //initialise arrays
+    let pool = [];
+    let home = [];
+    let away = [];
+
+    //initiaise document elements
+    let enterName = d.getElementById("enterName");
+    let submitName = d.getElementById("submitName");
+    let output = d.getElementById("output");
+    let generate = d.getElementById("generate");
+    let homeTeam = d.getElementById("homeTeam");
+    let awayTeam = d.getElementById("awayTeam");
+    let playerPool = d.getElementById("playerPool");
+
+    //New Player button listener
+    //adds name if array length below pool maximum
+    //display pool
+    submitName.addEventListener("click", () => {
+        pool.length < 10 ? pool.push(enterName.value) : output.textContent = "Pool full";
+        playerPool.textContent = pool;
+    })
+
+    //Generate Teams button listener
+    //calls teamsplitter function
+    generate.addEventListener("click", ()=>{
+        teamSplitter();
+    })
+
+    //generate a random integer below the max permissable value
+    function generateRandom(min, max) {
+        return Math.ceil( Math.random() * ( max - min ) );
+    }
+        
+    //populate array with random integers
+    //test that integer does not already exist
+    //return array
+    function generatePlayerIndex(players) {
+        let playerNumbers = [];
+        while ( playerNumbers.length < players ) {
+            var newIndex = (generateRandom(0, players)) - 1;
+            if (playerNumbers.indexOf(newIndex) == -1) {
             playerNumbers.push(newIndex);
         }
     }
-    return playerNumbers;
-}
-
-// declare pool array of players - note this will be refactored to accept user input
-// loop through random player Index pool and push odd numbers to blue and even numbers to red
-function teamSplitter() {
-    let pool = [
-        "Jack Daniels",
-        "Jim Beam",
-        "Tony Stark",
-        "Levi Strauss",
-        "Calvin Klein",
-        "Freddy Krueger",
-        "Jason Vorhees",
-        "Michael Myers",
-        "Tony Montana",
-        "Nino Brown"
-    ];
-    
-    let reds = [];
-    let blues = [];
-
-    let playerIndex = generatePlayerIndex(pool.length);
-    console.log(playerIndex);
-
-    for ( i = 0; i < (pool.length); i++ ) {
-       playerIndex[i] % 2 === 0 ? reds.push(pool[playerIndex[i]]) : blues.push(pool[playerIndex[i]]);
+        return playerNumbers;
     }
-
-    console.log(reds);
-    console.log(blues);
-}
-
-teamSplitter();
+    
+    //assign randomly generated index to new array
+    //push to home or away based on whether the index is odd or even
+    //output teams to page
+    function teamSplitter() {
+        
+        let playerIndex = generatePlayerIndex(pool.length);
+         
+        for ( i = 0; i < (pool.length); i++ ) {
+            playerIndex[i] % 2 === 0 ? home.push(pool[playerIndex[i]]) : away.push(pool[playerIndex[i]]);
+        }
+        
+       homeTeam.textContent = home;
+       awayTeam.textContent = away;
+    }
+    
+})(document)
+ 
